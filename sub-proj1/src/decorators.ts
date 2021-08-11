2fe4ccaa976bece09f870f2bb5f2b5f0faadaba9
Kristina Pchelintseva <39121951+kricact@users.noreply.github.com>
2021-08-11 19:25:11 +0300

    Update decorators.ts

--- sub-proj1/src/decorators.ts
+++ sub-proj1/src/decorators.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-let memoizeId = 0;
 export function createMemoizer() {
 	const memoizeKeyPrefix = `$memoize${memoizeId++}`;
 	let self: any = undefined;
