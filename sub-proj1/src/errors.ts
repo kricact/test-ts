e5aed896c8491ee1c480c204426f936eaab4ee8a
Kristina Pchelintseva <39121951+kricact@users.noreply.github.com>
2021-08-11 19:16:36 +0300

    Update errors.ts

--- sub-proj1/src/errors.ts
+++ sub-proj1/src/errors.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+export const superValue = 22;
+
 export interface ErrorListenerCallback {
 	(error: any): void;
 }
